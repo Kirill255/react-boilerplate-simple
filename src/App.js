2e49922773d71fc42711e8c8693bcef3ffc64874
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-25 01:27:54 +0300

    add husky

--- src/App.js
+++ src/App.js
@@ -8,8 +8,10 @@ class App extends Component {
     count: 0
   };
 
-  increment = () => this.setState((prevState) => ({ count: prevState.count + 1 }));
-  decrement = () => this.setState((prevState) => ({ count: prevState.count - 1 }));
+  increment = () =>
+    this.setState(prevState => ({ count: prevState.count + 1 }));
+  decrement = () =>
+    this.setState(prevState => ({ count: prevState.count - 1 }));
 
   render() {
     const { count } = this.state;
