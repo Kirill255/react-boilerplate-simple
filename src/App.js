ca32a827832db8b0406f131c0c142bd7f5b87d9c
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-24 18:56:56 +0300

    add @babel/plugin-proposal-class-properties

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
 
 export default class App extends Component {
+  state = {
+    count: 0
+  };
+
   render() {
-    // debugger;
-    return <h1>Hello world!!!</h1>;
+    return (
+      <div>
+        <h1>Hello world!!!</h1>
+        <h2>Count: {this.state.count}</h2>
+        <button onClick={() => this.setState((prevState) => ({ count: prevState.count - 1 }))}>
+          -
+        </button>
+        <button onClick={() => this.setState((prevState) => ({ count: prevState.count + 1 }))}>
+          +
+        </button>
+      </div>
+    );
   }
 }
