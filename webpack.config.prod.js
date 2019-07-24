b24784240ce1dd0bded73e28e464d3dce6d9fd25
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-07-24 20:04:30 +0300

    add webpack-bundle-analyzer

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,6 +1,14 @@
 const merge = require("webpack-merge");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const baseConfig = require("./webpack.config.base");
 
 module.exports = merge(baseConfig, {
-  mode: "production"
+  mode: "production",
+  plugins: [
+    new BundleAnalyzerPlugin({
+      analyzerMode: "static", // in static mode single HTML file with bundle report will be generated
+      openAnalyzer: false, // default: true -> automatically open report in default browser.
+      reportFilename: "bundle_sizes.html" // default report.html
+    })
+  ]
 });
